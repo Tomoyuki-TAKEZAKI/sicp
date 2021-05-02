95128b66e42a3e5f56ceaa2b94edc4b03ad4b5f0
Tomoyuki TAKEZAKI <t-takezaki@unext.jp>
2021-05-02 14:59:16 +0900

    average, not sum

--- ch1/ch1.scm
+++ ch1/ch1.scm
@@ -726,9 +726,10 @@
 ;;; EXERCISE 1.44
 
 (define (smooth f)
-  (lambda (x) (+ (f (- x dx))
-                 (f x)
-                 (f (+ x dx)))))
+  (lambda (x) (/ (+ (f (- x dx))
+                    (f x)
+                    (f (+ x dx)))
+                 3)))
 
 (define (n-fold-smooth f n)
   (repeated (smooth f) n))
